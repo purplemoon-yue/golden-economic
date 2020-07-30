007b9df830b6dd4b39c0a8fda3fef27e60c0858f
xiaoyue <xiaoyue@qq.com>
2020-07-30 21:35:55 +0800

    第一次全部提交

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <br><br>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <br><br>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return {
+                input:''
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
